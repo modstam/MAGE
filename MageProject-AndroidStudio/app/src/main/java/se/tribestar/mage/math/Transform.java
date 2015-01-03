879f76575b934cbd85c8d6d1dc2db5d04e088eac
modstam <modstam@live.com>
2015-01-03 15:54:39 +0100

    math package

---
+++ MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/math/Transform.java
@@ -0,0 +1,12 @@
+package se.tribestar.mage.math;
+
+/**
+ * Created by modstam on 2015-01-03.
+ */
+public class Transform {
+    public Vector3 position;
+    public Vector3 rotation;
+    public Vector3 scale;
+
+
+}
---
+++ MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/math/Vector2.java
@@ -0,0 +1,14 @@
+package se.tribestar.mage.math;
+
+/**
+ * Created by modstam on 2015-01-03.
+ */
+public class Vector2 {
+    public float x;
+    public float y;
+
+    public Vector2(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+}
