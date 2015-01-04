fbfeea71a6d47883c5913ab13f01b32c826b33e7
Stjerndal <stjernd@kth.se>
2015-01-04 18:47:25 +0100

    Created class for interacting with android keyboard

---
+++ MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/backend/android/KeyboardHandler.java
@@ -0,0 +1,75 @@
+package se.tribestar.mage.backend.android;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.view.View;
+import android.view.View.OnKeyListener;
+
+import se.tribestar.mage.backend.Input.KeyEvent;
+import se.tribestar.mage.backend.Pool;
+
+/**
+ * Created by Andreas Stjerndal on 04-Jan-2015.
+ */
+public class KeyboardHandler implements OnKeyListener {
+    boolean[] pressedKeys = new boolean[128];
+    Pool<KeyEvent> keyEventPool;
+    List<KeyEvent> keyEventsBuffer = new ArrayList<KeyEvent>();
+    List<KeyEvent> keyEvents = new ArrayList<KeyEvent>();
+
+    public KeyboardHandler(View view) {
+        Pool.PoolObjectFactory<KeyEvent> factory = new Pool.PoolObjectFactory<KeyEvent>() {
+            public KeyEvent createObject() {
+                return new KeyEvent();
+            }
+        };
+        keyEventPool = new Pool<KeyEvent>(factory, 100);
+        view.setOnKeyListener(this);
+        view.setFocusableInTouchMode(true);
+        view.requestFocus();
+    }
+
+    public boolean onKey(View v, int keyCode, android.view.KeyEvent event) {
+        if (event.getAction() == android.view.KeyEvent.ACTION_MULTIPLE)
+            return false;
+
+        synchronized (this) {
+            KeyEvent keyEvent = keyEventPool.newObject();
+            keyEvent.keyCode = keyCode;
+            keyEvent.keyChar = (char) event.getUnicodeChar();
+            if (event.getAction() == android.view.KeyEvent.ACTION_DOWN) {
+                keyEvent.type = KeyEvent.KEY_DOWN;
+                if(keyCode > 0 && keyCode < 127)
+                    pressedKeys[keyCode] = true;
+            }
+            if (event.getAction() == android.view.KeyEvent.ACTION_UP) {
+                keyEvent.type = KeyEvent.KEY_UP;
+                if(keyCode > 0 && keyCode < 127)
+                    pressedKeys[keyCode] = false;
+            }
+            keyEventsBuffer.add(keyEvent);
+        }
+        return false;
+    }
+
+    public boolean isKeyPressed(int keyCode) {
+        if (keyCode < 0 || keyCode > 127)
+            return false;
+        return pressedKeys[keyCode];
+    }
+
+    public List<KeyEvent> getKeyEvents() {
+        synchronized (this) {
+            int len = keyEvents.size();
+            for (int i = 0; i < len; i++) {
+                keyEventPool.free(keyEvents.get(i));
+            }
+            keyEvents.clear();
+            keyEvents.addAll(keyEventsBuffer);
+            keyEventsBuffer.clear();
+            return keyEvents;
+        }
+    }
+}
+
