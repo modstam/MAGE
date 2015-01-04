d6965fe64081a2ee0cbd04800741af25e7d04682
Stjerndal <stjernd@kth.se>
2015-01-04 18:47:01 +0100

    Creating a class for interaction with Accelerometer

---
+++ MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/backend/android/AccelerometerHandler.java
@@ -0,0 +1,49 @@
+package se.tribestar.mage.backend.android;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+/**
+ * Created by Andreas Stjerndal on 04-Jan-2015.
+ */
+public class AccelerometerHandler implements SensorEventListener {
+    float accelX;
+    float accelY;
+    float accelZ;
+
+    public AccelerometerHandler(Context context) {
+        SensorManager manager = (SensorManager) context
+                .getSystemService(Context.SENSOR_SERVICE);
+        if (manager.getSensorList(Sensor.TYPE_ACCELEROMETER).size() != 0) {
+            Sensor accelerometer = manager.getSensorList(
+                    Sensor.TYPE_ACCELEROMETER).get(0);
+            manager.registerListener(this, accelerometer,
+                    SensorManager.SENSOR_DELAY_GAME);
+        }
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // nothing to do here
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        accelX = event.values[0];
+        accelY = event.values[1];
+        accelZ = event.values[2];
+    }
+
+    public float getAccelX() {
+        return accelX;
+    }
+
+    public float getAccelY() {
+        return accelY;
+    }
+
+    public float getAccelZ() {
+        return accelZ;
+    }
+}
