32692593976cc57542733be93da5d7311accc18f
Stjerndal <stjernd@kth.se>
2015-01-04 18:53:12 +0100

    Implementing AndroidInput class.

--- MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/backend/android/AndroidInput.java
+++ MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/backend/android/AndroidInput.java
@@ -1,7 +1,63 @@
 package se.tribestar.mage.backend.android;
 
+import java.util.List;
+
+import android.content.Context;
+import android.os.Build.VERSION;
+import android.view.View;
+
+import se.tribestar.mage.backend.Input;
+
 /**
  * Created by Andreas Stjerndal on 03-Jan-2015.
  */
-public class AndroidInput {
+public class AndroidInput implements Input {
+    AccelerometerHandler accelHandler;
+    KeyboardHandler keyHandler;
+    TouchHandler touchHandler;
+
+    public AndroidInput(Context context, View view, float scaleX, float scaleY) {
+        accelHandler = new AccelerometerHandler(context);
+        keyHandler = new KeyboardHandler(view);
+        if (Integer.parseInt(VERSION.SDK) < 5)
+            touchHandler = new SingleTouchHandler(view, scaleX, scaleY);
+        else
+            touchHandler = new MultiTouchHandler(view, scaleX, scaleY);
+    }
+
+    public boolean isKeyPressed(int keyCode) {
+        return keyHandler.isKeyPressed(keyCode);
+    }
+
+    public boolean isTouchDown(int pointer) {
+        return touchHandler.isTouchDown(pointer);
+    }
+
+    public int getTouchX(int pointer) {
+        return touchHandler.getTouchX(pointer);
+    }
+
+    public int getTouchY(int pointer) {
+        return touchHandler.getTouchY(pointer);
+    }
+
+    public float getAccelX() {
+        return accelHandler.getAccelX();
+    }
+
+    public float getAccelY() {
+        return accelHandler.getAccelY();
+    }
+
+    public float getAccelZ() {
+        return accelHandler.getAccelZ();
+    }
+
+    public List<TouchEvent> getTouchEvents() {
+        return touchHandler.getTouchEvents();
+    }
+
+    public List<KeyEvent> getKeyEvents() {
+        return keyHandler.getKeyEvents();
+    }
 }
