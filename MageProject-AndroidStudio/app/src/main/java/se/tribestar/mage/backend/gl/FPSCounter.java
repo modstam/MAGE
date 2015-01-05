ba4826f64ff9fafcda6f2c01528824e70df34f62
Stjerndal <stjernd@kth.se>
2015-01-05 14:57:11 +0100

    FPS Counting class.

---
+++ MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/backend/gl/FPSCounter.java
@@ -0,0 +1,22 @@
+package se.tribestar.mage.backend.gl;
+
+import android.util.Log;
+
+/**
+ * Call logFrame() once per frame to output FPS in log.
+ * 
+ * Created by Andreas Stjerndal on 05-Jan-2015.
+ */
+public class FPSCounter {
+    long startTime = System.nanoTime();
+    int frames = 0;
+
+    public void logFrame() {
+        frames++;
+        if(System.nanoTime() - startTime >= 1000000000) {
+            Log.d("FPSCounter", "fps: " + frames);
+            frames = 0;
+            startTime = System.nanoTime();
+        }
+    }
+}
