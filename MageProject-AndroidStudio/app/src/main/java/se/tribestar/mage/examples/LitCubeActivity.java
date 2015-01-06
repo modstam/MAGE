a17e8f874ec3d13447490863d21e359cf06694a9
Stjerndal <stjernd@kth.se>
2015-01-06 15:49:00 +0100

    Cleaning up code in LitCubeActivity.

--- MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/examples/LitCubeActivity.java
+++ MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/examples/LitCubeActivity.java
@@ -1,14 +1,13 @@
 package se.tribestar.mage.examples;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.opengl.GLU;
 
-import se.tribestar.mage.R;
-import se.tribestar.mage.backend.GLWorld;
+import javax.microedition.khronos.opengles.GL10;
+
+import se.tribestar.mage.backend.gl.GLWorld;
 import se.tribestar.mage.backend.gl.DirectionalLight;
 import se.tribestar.mage.backend.gl.GLBackendController;
+import se.tribestar.mage.backend.gl.Material;
 import se.tribestar.mage.backend.gl.Texture;
 import se.tribestar.mage.backend.gl.Vertices3;
 
@@ -19,6 +18,8 @@ public class LitCubeActivity extends GLBackendController {
     }
 
     class LightScreen extends GLWorld {
+        public final String TEXTURE_FILENAME = "crate.png";
+
         float angle;
         Vertices3 cube;
         Texture texture;
@@ -31,7 +32,7 @@ public class LitCubeActivity extends GLBackendController {
             super(controller);
 
             cube = createCube();
-            texture = new Texture(glGame, "crate.png");
+            texture = new Texture(controller, "crate.png");
             //ambientLight = new AmbientLight();
             //ambientLight.setColor(0, 0.2f, 0, 1);
             //pointLight = new PointLight();
@@ -85,7 +86,7 @@ public class LitCubeActivity extends GLBackendController {
                     16, 17, 18, 18, 19, 16,
                     20, 21, 22, 22, 23, 20,
                     24, 25, 26, 26, 27, 24 };
-            Vertices3 cube = new Vertices3(glGraphics, vertices.length / 8, indices.length, false, true, true);
+            Vertices3 cube = new Vertices3(getGLGraphics(), vertices.length / 8, indices.length, false, true, true);
             cube.setVertices(vertices, 0, vertices.length);
             cube.setIndices(indices, 0, indices.length);
             return cube;
@@ -98,24 +99,24 @@ public class LitCubeActivity extends GLBackendController {
 
         @Override
         public void present(float deltaTime) {
-            GL10 gl = glGraphics.getGL();
+            GL10 gl = getGLGraphics().getGL();
             gl.glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
             gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
             gl.glEnable(GL10.GL_DEPTH_TEST);
-            gl.glViewport(0, 0, glGraphics.getWidth(), glGraphics.getHeight());
+            gl.glViewport(0, 0, getGLGraphics().getWidth(), getGLGraphics().getHeight());
 
             gl.glMatrixMode(GL10.GL_PROJECTION);
             gl.glLoadIdentity();
-            GLU.gluPerspective(gl, 67, glGraphics.getWidth()
-                    / (float) glGraphics.getHeight(), 0.1f, 10f);
+            GLU.gluPerspective(gl, 67, getGLGraphics().getWidth()
+                    / (float) getGLGraphics().getHeight(), 0.1f, 10f);
             gl.glMatrixMode(GL10.GL_MODELVIEW);
             gl.glLoadIdentity();
             GLU.gluLookAt(gl, 0, 1, 3, 0, 0, 0, 0, 1, 0);
 
             gl.glEnable(GL10.GL_LIGHTING);
 
-            ambientLight.enable(gl);
-            pointLight.enable(gl, GL10.GL_LIGHT0);
+            //ambientLight.enable(gl);
+            //pointLight.enable(gl, GL10.GL_LIGHT0);
             directionalLight.enable(gl, GL10.GL_LIGHT1);
             material.enable(gl);
 
@@ -127,7 +128,7 @@ public class LitCubeActivity extends GLBackendController {
             cube.draw(GL10.GL_TRIANGLES, 0, 6 * 2 * 3);
             cube.unbind();
 
-            pointLight.disable(gl);
+            //pointLight.disable(gl);
             directionalLight.disable(gl);
 
             gl.glDisable(GL10.GL_TEXTURE_2D);
