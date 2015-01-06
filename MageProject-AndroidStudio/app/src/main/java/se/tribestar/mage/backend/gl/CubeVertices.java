f93ae7ebbe9be306195ae1a4e2d1fe4835b229f7
modstam <modstam@live.com>
2015-01-06 15:31:32 +0100

    cubevertices constructor

--- MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/backend/gl/CubeVertices.java
+++ MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/backend/gl/CubeVertices.java
@@ -3,5 +3,54 @@ package se.tribestar.mage.backend.gl;
 /**
  * Created by Andreas Stjerndal on 06-Jan-2015.
  */
-public class CubeVertices {
+public class CubeVertices extends Vertices3{
+
+
+    public CubeVertices(GLGraphics glGraphics, int maxVertices, int maxIndices, boolean hasColor, boolean hasTexCoords, boolean hasNormals) {
+        super(glGraphics,maxVertices,maxIndices,hasColor,hasTexCoords,hasNormals);
+    }
+
+    public CubeVertices(GLGraphics glGraphics){
+
+        super(glGraphics,24,36,true,true,true);
+
+        float[] vertices = { −0.5f, -0.5f, 0.5f, 0, 1,
+                0.5f, -0.5f, 0.5f, 1, 1,
+                0.5f, 0.5f, 0.5f, 1, 0,
+                -0.5f, 0.5f, 0.5f, 0, 0,
+                0.5f, -0.5f, 0.5f, 0, 1,
+                0.5f, -0.5f, -0.5f, 1, 1,
+                0.5f, 0.5f, -0.5f, 1, 0,
+                0.5f, 0.5f, 0.5f, 0, 0,
+                0.5f, -0.5f, -0.5f, 0, 1,
+                -0.5f, -0.5f, -0.5f, 1, 1,
+                -0.5f, 0.5f, -0.5f, 1, 0,
+                0.5f, 0.5f, -0.5f, 0, 0,
+                -0.5f, -0.5f, -0.5f, 0, 1,
+                -0.5f, -0.5f, 0.5f, 1, 1,
+                -0.5f, 0.5f, 0.5f, 1, 0,
+                -0.5f, 0.5f, -0.5f, 0, 0,
+                -0.5f, 0.5f, 0.5f, 0, 1,
+                0.5f, 0.5f, 0.5f, 1, 1,
+                0.5f, 0.5f, -0.5f, 1, 0,
+                -0.5f, 0.5f, -0.5f, 0, 0,
+                -0.5f, -0.5f, 0.5f, 0, 1,
+                0.5f, -0.5f, 0.5f, 1, 1,
+                0.5f, -0.5f, -0.5f, 1, 0,
+                -0.5f, -0.5f, -0.5f, 0, 0
+        };
+        short[] indices = { 0, 1, 3, 1, 2, 3,
+                4, 5, 7, 5, 6, 7,
+                8, 9, 11, 9, 10, 11,
+                12, 13, 15, 13, 14, 15,
+                16, 17, 19, 17, 18, 19,
+                20, 21, 23, 21, 22, 23,
+        };
+        setVertices(vertices,0,vertices.length);
+        setIndices(indices,0,indices.length);
+        
+    }
+
+
+
 }
---
+++ MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/backend/gl/Renderer.java
@@ -0,0 +1,13 @@
+package se.tribestar.mage.backend.gl;
+
+import se.tribestar.mage.world.drawable.Drawable;
+
+/**
+ * Created by modstam on 2015-01-06.
+ */
+public class Renderer {
+
+    public void draw(Drawable drawable, Vertices3 vertices){
+
+    }
+}
