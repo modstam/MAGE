e1568b820204f0361e5a0d60ac252abe3fff5588
Stjerndal <stjernd@kth.se>
2015-01-07 19:07:09 +0100

    Adding constructor and fields to MageGame.

--- MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/frontend/MageGame.java
+++ MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/frontend/MageGame.java
@@ -1,9 +1,24 @@
 package se.tribestar.mage.frontend;
 
+import se.tribestar.mage.backend.gl.GLBackendController;
+import se.tribestar.mage.backend.gl.GLWorld;
+import se.tribestar.mage.world.World;
+
 /**
  * Users of the engine expands this abstract class with the main class of their game.
  *
  * Created by Andreas Stjerndal on 07-Jan-2015.
  */
 public abstract class MageGame {
+    GLBackendController controller;
+    World world;
+
+    public MageGame() {
+        controller = new GLBackendController() {
+            @Override
+            public GLWorld getWorld() {
+                return world;
+            }
+        };
+    }
 }
