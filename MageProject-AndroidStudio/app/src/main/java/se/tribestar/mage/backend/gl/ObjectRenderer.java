849665665ac9a7668fe1d66f83a159339f005dd7
modstam <modstam@live.com>
2015-01-07 20:20:21 +0100

    some skeleton for renderer

--- MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/backend/gl/ObjectRenderer.java
+++ MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/backend/gl/ObjectRenderer.java
@@ -2,37 +2,88 @@ package se.tribestar.mage.backend.gl;
 
 import android.opengl.GLU;
 
+import java.util.List;
+
 import javax.microedition.khronos.opengles.GL10;
 
 import se.tribestar.mage.world.drawable.Drawable;
+import se.tribestar.mage.world.light.Light;
+import se.tribestar.mage.world.viewport.Camera;
+import se.tribestar.mage.world.viewport.ViewPort;
 
 /**
  * Created by modstam on 2015-01-06.
  */
 public class ObjectRenderer {
 
-    public void draw(Drawable drawable, Vertices3 vertices){
 
-        GLGraphics glGraphics = vertices.getGL();
+    public void prerender(List<Light> lights, List<ViewPort> viewPorts, GLGraphics glGraphics){
         GL10 gl = glGraphics.getGL();
+
+        //TEMP------------------------
         gl.glViewport(0, 0, glGraphics.getWidth(), glGraphics.getHeight());
-        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+
+        clearFrame(glGraphics);
+
         gl.glMatrixMode(GL10.GL_PROJECTION);
         gl.glLoadIdentity();
         GLU.gluPerspective(gl, 67,
                 glGraphics.getWidth() / (float) glGraphics.getHeight(),
                 0.1f, 10.0f);
+        //----------------------------
+
+    }
+
+    public void draw(Drawable drawable, Vertices3 vertices){
+
+        GLGraphics glGraphics = vertices.getGL();
+        GL10 gl = glGraphics.getGL();
+
+        clearFrame(glGraphics);
+
         gl.glMatrixMode(GL10.GL_MODELVIEW);
         gl.glLoadIdentity();
         gl.glEnable(GL10.GL_DEPTH_TEST);
         //gl.glEnable(GL10.GL_TEXTURE_2D);
         //texture.bind();
+        setMaterial(drawable, glGraphics);
         vertices.bind();
-        gl.glTranslatef(0,0,-3);
+        setWorldPosition(drawable, glGraphics);
         //gl.glRotatef(angle, 0, 1, 0);
         vertices.draw(GL10.GL_TRIANGLES, 0, 36);
         vertices.unbind();
         //gl.glDisable(GL10.GL_TEXTURE_2D);
         gl.glDisable(GL10.GL_DEPTH_TEST);
+
     }
+
+
+    public void clearFrame(GLGraphics glGraphics){
+        GL10 gl = glGraphics.getGL();
+        gl.glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+
+    }
+
+    public void setMaterial(Drawable drawable, GLGraphics glGraphics){
+        if(drawable.material != null){
+            drawable.material.enable(glGraphics.getGL());
+        }
+    }
+
+    public void setWorldPosition(Drawable d, GLGraphics glGraphics){
+        GL10 gl = glGraphics.getGL();
+        float x = d.transform.position.x;
+        float y = d.transform.position.y;
+        float z = d.transform.position.z;
+
+        gl.glTranslatef(x,y,z);
+    }
+
+    public void setWorldRotation(Drawable d, GLGraphics glGraphics){
+        GL10 gl = glGraphics.getGL();
+            //TODO
+    }
+
+
 }
