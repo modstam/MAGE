684f15abd28318e397fb848c4163d32848bc90de
Stjerndal <stjernd@kth.se>
2015-01-07 18:01:12 +0100

    Created front end user game abstract class.

---
+++ MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/frontend/MageGame.java
@@ -0,0 +1,9 @@
+package se.tribestar.mage.frontend;
+
+/**
+ * Users of the engine expands this abstract class with the main class of their game.
+ *
+ * Created by Andreas Stjerndal on 07-Jan-2015.
+ */
+public abstract class MageGame {
+}
