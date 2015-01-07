1b6c2dd2473d72132f5be3601f02bc037930a913
Stjerndal <stjernd@kth.se>
2015-01-07 19:16:09 +0100

    Adding call to super constructor.

--- MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/frontend/MageGame.java
+++ MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/frontend/MageGame.java
@@ -5,16 +5,15 @@ import se.tribestar.mage.backend.gl.GLWorld;
 import se.tribestar.mage.world.World;
 
 /**
- * Users of the engine expands this abstract class with the main class of their game.
+ * Users of the engine expands this class with the main class of their game.
  *
  * Created by Andreas Stjerndal on 07-Jan-2015.
  */
-public abstract class MageGame extends GLBackendController {
+public class MageGame extends GLBackendController {
     World world;
 
     public MageGame() {
-
-
+        super();
     }
 
     @Override
