280dcbb97365652f6999bd698d17ffc9117c6d04
Stjerndal <stjernd@kth.se>
2015-01-09 17:14:53 +0100

    Class for testing a textured cube.

---
+++ MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/examples/TexturedCube.java
@@ -0,0 +1,39 @@
+package se.tribestar.mage.examples;
+
+import se.tribestar.mage.logic.Logic;
+import se.tribestar.mage.math.Vector3;
+import se.tribestar.mage.world.World;
+import se.tribestar.mage.world.drawable.Cube;
+import se.tribestar.mage.world.drawable.visuals.Color;
+import se.tribestar.mage.world.light.DirectionalLight;
+
+/**
+ * Created by modstam on 2015-01-07.
+ */
+public class TexturedCube extends Logic {
+
+    float angle = 20f;
+    Cube cube;
+
+    public TexturedCube(World world){
+        super(world);
+
+        cube = new Cube();
+        cube.transform.position = new Vector3(0,0,0);
+        cube.transform.rotation = new Vector3(0,0,0);
+        cube.setTexturePath("crate.png");
+        world.addObject(cube);
+
+        DirectionalLight dLight = new DirectionalLight();
+        dLight.ambientColor = new Color(1,1,1,1);
+        dLight.diffuseColor = new Color(1,1,1,1);
+        dLight.direction = new Vector3(1,0,0);
+        world.addLight(dLight);
+    }
+
+    @Override
+    public void update(float deltaTime){
+        cube.transform.rotation.x += angle*deltaTime;
+        cube.transform.rotation.y += angle*deltaTime;
+    }
+}
