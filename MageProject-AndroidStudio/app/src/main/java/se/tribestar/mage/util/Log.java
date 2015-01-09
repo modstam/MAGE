77b83526f23e477294130fe3c11679382589c569
Stjerndal <stjernd@kth.se>
2015-01-09 17:12:34 +0100

    Added Log utility class and package.

---
+++ MageProject-AndroidStudio/app/src/main/java/se/tribestar/mage/util/Log.java
@@ -0,0 +1,11 @@
+package se.tribestar.mage.util;
+
+/**
+ * Created by Andreas Stjerndal on 09-Jan-2015.
+ */
+public class Log {
+    public static final String MAGE_LOG_TAG = "MAGE";
+    public static void log(String msg) {
+        android.util.Log.d(MAGE_LOG_TAG, msg);
+    }
+}
